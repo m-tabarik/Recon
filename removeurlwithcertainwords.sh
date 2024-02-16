489d8f9926031a492ed44550cb7733e4fa09fa7f
mtabarikasif <79154025+m-tabarik@users.noreply.github.com>
2024-02-16 19:30:54 +0500

    Create removeurlwithcertainwords.sh
    input a list of subdomains and it will remove the urls containing specific keywords like shop, test, production etc. Than saves the rest of the urls to new file.

---
+++ removeurlwithcertainwords.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+input_file="$1"
+output_file="${input_file%.txt}_filtered.txt"
+
+if [ ! -f "$input_file" ]; then
+  echo "Error: Input file not found."
+  exit 1
+fi
+
+while IFS= read -r line; do
+  if [[ $line != *"shop"* ]]; then
+    echo "$line" >> "$output_file"
+  fi
+done < "$input_file"
+
+echo "URLs with 'shop' keyword removed and saved to $output_file"
+
